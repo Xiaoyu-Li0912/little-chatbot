9c36452bcd2cb053979ba425f26c5e13c79015e7
Xiaoyu-Li0912 <xli083@connect.hkust-gz.edu.cn>
2026-03-09 12:54:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@
 MODEL_ID = "Qwen/Qwen2.5-1.5B-Instruct"
 
 # 数据集路径
-DATA_PATH = "data/finetune_liujiayan_v2.jsonl"
+DATA_PATH = "data/finetune_v2.jsonl"
 
 # 模型输出路径 (建议加上日期或参数标识，方便区分)
-OUTPUT_DIR = "output/Qwen2.5-LiuJiayan-LoRA-v1"
+OUTPUT_DIR = "output/Qwen2.5-LoRA-v1"
 
 # 名称
 USER_NAME = "XY. Lee"
-TARGET_NAME = "刘嘉岩"
+TARGET_NAME = "小明"
 # ================= 训练超参数 (Training Arguments) =================
 # 这些参数会直接传给 Transformers 的 TrainingArguments
 TRAIN_ARGS = {
@@ -49,17 +49,17 @@ MAX_SEQ_LENGTH = 1024
 # ================= 系统提示词 (System Prompt) =================
 # 把它放在配置里，保证训练和推理使用同一套人设
 SYSTEM_PROMPT = (
-    "你现在是刘嘉岩。你是一个性格比较直率、有点暴躁、喜欢互损的男生。"
+    "你现在是XY.Lee的朋友，你的名字是小明。你是一个性格比较直率、有点暴躁、喜欢互损的男生。"
     "你喜欢玩怪物猎人、永劫无间和我的世界。"
-    "请用刘嘉岩的语气回复XY. Lee的消息。"
+    "请用小明的语气回复XY. Lee的消息。"
 )
 
 
 
 # # ================= 基础路径配置 =================
 # MODEL_ID = "Qwen/Qwen2.5-1.5B-Instruct"
-# DATA_PATH = "data/finetune_liujiayan_v2.jsonl"
-# OUTPUT_DIR = "output/Qwen2.5-LiuJiayan-Full-v1"
+# DATA_PATH = "data/finetune_v2.jsonl"
+# OUTPUT_DIR = "output/Qwen2.5-Full-v1"
 
 # # ================= 训练超参数（全量微调专用） =================
 # TRAIN_ARGS = {
@@ -77,5 +77,6 @@ SYSTEM_PROMPT = (
 #     "你的口头禅包括'cnm'、'sb'、'OK?'、'。。。。'、'再发个'。"
 #     "请用刘嘉岩的语气回复XY. Lee的消息，回复要简短、口语化，符合人物设定。"
 # )
+
 
 # # 删掉所有LoRA相关配置（全量微调不用）
